bdecc6a5ee4bb81dadb1f8c40abf3ba65e3933a9
DrackDMG <drackmg@gmail.com>
2025-10-07 17:00:33 -0600

    test dmg-side-menu

--- projects/dmg-side-menu/src/lib/dmg-side-menu.spec.ts
+++ projects/dmg-side-menu/src/lib/dmg-side-menu.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DmgSideMenu } from './dmg-side-menu';
 import { provideZonelessChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
 
 describe('DmgSideMenu', () => {
   let component: DmgSideMenu;
@@ -10,7 +11,7 @@ describe('DmgSideMenu', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [DmgSideMenu],
-      providers: [provideZonelessChangeDetection()]
+      providers: [provideZonelessChangeDetection(), provideRouter([])]
     })
     .compileComponents();
 
@@ -21,5 +22,39 @@ describe('DmgSideMenu', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+   it('should call onSingIn when button is clicked', () => {
+    spyOn(component.onSignIn, 'emit');
+    fixture.componentRef.setInput('isAuthenticated', false);
+
+    fixture.detectChanges();
+
+    const button = fixture.nativeElement.querySelector(
+      '[data-login]'
+    ) as HTMLButtonElement;
+
+    expect(button).toBeTruthy();
+
+    button.click();
+
+    expect(component.onSignIn.emit).toHaveBeenCalled();
+  });
+
+  it('should call onSignOut when button is clicked', () => {
+    spyOn(component.onSignOut, 'emit');
+    fixture.componentRef.setInput('isAuthenticated', true);
+
+    fixture.detectChanges();
+
+    const button = fixture.nativeElement.querySelector(
+      '[data-logout]'
+    ) as HTMLButtonElement;
+
+    expect(button).toBeTruthy();
+
+    button.click();
+
+    expect(component.onSignOut.emit).toHaveBeenCalled();
   });
 });
