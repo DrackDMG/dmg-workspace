83c660424b419d570a1b06e1ef134d1d0126b9ab
DrackDMG <drackmg@gmail.com>
2025-10-07 15:24:39 -0600

    funcionalidades y estilos

--- projects/dmg-side-menu/src/lib/dmg-side-menu.ts
+++ projects/dmg-side-menu/src/lib/dmg-side-menu.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
+export enum TitleColor {
+  red = 'text-red-500',
+  green = 'text-green-500',
+  blue = 'text-blue-500',
+  purple = 'text-purple-500',
+}
 @Component({
   selector: 'lib-dmg-side-menu',
-  imports: [],
-  template: `
-    <p>
-      dmg-side-menu works!
-    </p>
-  `,
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './dmg-side-menu.html',
   styles: ``
 })
 export class DmgSideMenu {
+  isAuthenticated = input(false);
 
+  titleColor = input<TitleColor>(TitleColor.purple);
+
+  onSignOut = output();
+  onSignIn = output();
 }
--- projects/dmg-testbed-app/src/app/app.ts
+++ projects/dmg-testbed-app/src/app/app.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DmgSideMenu } from 'dmg-side-menu';
+import { DmgSideMenu, TitleColor } from 'dmg-side-menu';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, DmgSideMenu],
@@ -8,5 +8,13 @@ import { DmgSideMenu } from 'dmg-side-menu';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('dmg-testbed-app');
+  isAuthenticated = signal(true);
+  onSignOut() {
+    this.isAuthenticated.set(false);
+  }
+  onSignIn() {
+    this.isAuthenticated.set(true);
+  }
+
+  TitleColor =  TitleColor
 }
